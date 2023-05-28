4937256e892d8e064598077b911a31ceb417f5ce
Ben Williams <8enwilliams@gmail.com>
2023-05-28 18:32:21 +1000

    include errors in response exception message

--- Benjineering.Responses/Exceptions/ResponseException.cs
+++ Benjineering.Responses/Exceptions/ResponseException.cs
@@ -5,8 +5,31 @@ public class ResponseException : Exception
     public readonly IResponse Response;
 
     public ResponseException(IResponse response, string? message = null) 
-        : base(message == null ? response.Message : $"{message}: {response.Message}")
+        : base(BuildMessage(response, message))
     {
         Response = response;
+    }
+
+    private static string BuildMessage(IResponse response, string? message = null)
+    {
+        var str = message == null ? response.Message : $"{message}: {response.Message}";
+
+        if (response.ValidationErrors.Any())
+        {
+            var validationErrors = response.ValidationErrors
+                .Select(x => $"\n\t\t{x.PropertyName}: {string.Join(string.Empty, x.Errors.Select(x => $"\n\t\t\t{x.Message}"))}");
+
+            str += "\n\tValidationErrors:" + validationErrors;
+        }
+
+        if (response.Errors.Any())
+        {
+            var errors = response.Errors
+                .Select(x => $"\n\t\t{x.Message}");
+
+            str += "\n\tErrors:" + errors;
+        }
+
+        return str;
     }
 }
